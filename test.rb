66cd8df169c7959d9ee640f2c640558da31302d3
Daisuke Inoue <hibariyashiki@gmail.com>
2016-06-27 11:07:52 +0900

    testcodes

--- test.rb
+++ test.rb
@@ -2,6 +2,8 @@ require './my_date_time.rb'
 
 m = MyDateTime.now
 n = MyDateTime.new(2015, 6, 27, 10, 10, 10, Rational(9, 24))
+n = MyDateTime.new(2015, 9, 22, 10, 10, 10, Rational(9, 24))
+
 p m.rc_ajd.to_f 
 
 p MyDateTime::leap_year?(299)
@@ -12,5 +14,9 @@ p MyDateTime::leap_year?(4)
 p MyDateTime::year_length(299)
 p MyDateTime::year_length(399)
 
-p m.prop_rc_year
-p n.prop_rc_year
+p m.prop_rc_year_day
+p n.prop_rc_year_day
+
+p m.day_ratio.to_f
+p m.day_ratio_in_season.to_f
+p n.day_ratio_in_season.to_f
