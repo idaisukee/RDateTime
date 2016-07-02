c2607a26a22c5a7c81e6f0185d9434c58529b262
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-02 15:47:07 +0900

    Range.rb

---
+++ Range.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../r_date_time.rb',  __FILE__)
+require File.expand_path('../float.rb',  __FILE__)
+
+class Range
+	def each_n_gc_min(num)
+
+		@min = self.min
+		@max = self.max
+		@num = num
+
+		@i = @min
+		while @i < @max
+			yield(@i)
+			@i +=  Rational(@num, 24 * 60)
+		end
+		
+	end
+end
