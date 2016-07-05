1cfe051bb0bb0a4492512b9da0b1ea9a664b967f
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-05 11:22:03 +0900

    modified display.rb.

--- display.rb
+++ display.rb
@@ -5,4 +5,10 @@ require File.expand_path('../float.rb',  __FILE__)
 
 m = RDateTime.now
 str = "#{m.prop_rc_year}:#{m.season_ratio.to_f.e_floor(2).to_f}|#{m.prop_rc_day.to_f.e_floor(3).to_f}"
+
+prop_rc_year = m.prop_rc_year
+season_ratio = sprintf('%2.2f', m.season_ratio.to_f.e_floor(2).to_f)
+prop_rc_day = sprintf('%3.3f', m.prop_rc_day.to_f.e_floor(3).to_f)
+
+str = "#{prop_rc_year}:#{season_ratio}|#{prop_rc_day}"
 print str
