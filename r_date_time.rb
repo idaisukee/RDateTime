9665037dbaeebf7911723cdfd353e23502e63796
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-05 11:33:20 +0900

    use "class << self" notation.

--- r_date_time.rb
+++ r_date_time.rb
@@ -7,37 +7,94 @@ class RDateTime < DateTime
 
 	RC_EPOCH = self.new(1792, 9, 22, 0, 0, 0, Rational(1, 24))
 
-	def self::leap_year?(prop_rc_year)
-		@prop_rc_year = prop_rc_year
-		@rc_year = @prop_rc_year + 1
-		if @rc_year % 4 == 0 then
-			if @rc_year % 100 == 0 then
-				if @rc_year % 400 == 0 then
-					true
+
+
+	class << self
+
+		def leap_year?(prop_rc_year)
+			@prop_rc_year = prop_rc_year
+			@rc_year = @prop_rc_year + 1
+			if @rc_year % 4 == 0 then
+				if @rc_year % 100 == 0 then
+					if @rc_year % 400 == 0 then
+						true
+					else
+						false
+					end
 				else
-					false
+					true
 				end
 			else
-				true
+				false
 			end
-		else
-			false
-		end
-	end
-
-
-
-	def self::year_length(prop_rc_year)
-
-		@prop_rc_year = prop_rc_year
-
-		if self::leap_year?(@prop_rc_year) then
-			366
-		else
-			365
 		end
 
+
+
+		def prop_rc_to_rc_ajd(prop_rc_year, prop_rc_month, prop_rc_day, rc_hour, rc_min, rc_sec)
+			# this method seems to produce a displacement of 1 day.
+
+			rc_ajd = 0
+
+			rc_ajd += RDateTime::prop_rc_year_to_rc_ajd(prop_rc_year)
+
+			rc_ajd += 30 * prop_rc_month
+			rc_ajd += prop_rc_day
+			rc_ajd += Rational(rc_hour, 10)
+			rc_ajd += Rational(rc_min, 1000)
+			rc_ajd += Rational(rc_sec, 100000)
+
+			rc_ajd
+		end
+
+
+
+		def prop_rc_year_to_rc_ajd(prop_rc_year)
+
+			if prop_rc_year == 0 then
+				0
+			else
+				array = Range.new(0, prop_rc_year - 1).to_a
+				days = array.map do |i|
+					self::year_length(i)
+				end
+				days_sum = days.inject(:+)
+			end
+			
+		end
+
+
+
+		def from_ajd(ajd)
+			@ajd = ajd
+			@jd = @ajd + Rational(1, 2)
+			self.jd(@jd)
+		end
+
+
+
+		def from_rc_ajd(rc_ajd)
+			@rc_ajd = rc_ajd
+			@ajd = self::RC_EPOCH.ajd + @rc_ajd
+			self::from_ajd(@ajd)
+		end
+
+
+
+		def year_length(prop_rc_year)
+
+			@prop_rc_year = prop_rc_year
+
+			if self::leap_year?(@prop_rc_year) then
+				366
+			else
+				365
+			end
+
+		end
+
 	end
+
 
 	def rc_ajd
 
@@ -102,53 +159,6 @@ end
 
 
 
-def self::prop_rc_to_rc_ajd(prop_rc_year, prop_rc_month, prop_rc_day, rc_hour, rc_min, rc_sec)
-	# this method seems to produce a displacement of 1 day.
-
-	rc_ajd = 0
-
-	rc_ajd += RDateTime::prop_rc_year_to_rc_ajd(prop_rc_year)
-
-	rc_ajd += 30 * prop_rc_month
-	rc_ajd += prop_rc_day
-	rc_ajd += Rational(rc_hour, 10)
-	rc_ajd += Rational(rc_min, 1000)
-	rc_ajd += Rational(rc_sec, 100000)
-
-	rc_ajd
-end
-
-
-
-def self::prop_rc_year_to_rc_ajd(prop_rc_year)
-	if prop_rc_year == 0 then
-		0
-	else
-		array = Range.new(0, prop_rc_year - 1).to_a
-		days = array.map do |i|
-			self::year_length(i)
-		end
-		days_sum = days.inject(:+)
-	end
-	
-		
-end
-
-
-
-	def self.from_ajd(ajd)
-		@ajd = ajd
-		@jd = @ajd + Rational(1, 2)
-		self.jd(@jd)
-	end
-
-
-
-	def self::from_rc_ajd(rc_ajd)
-		@rc_ajd = rc_ajd
-		@ajd = self::RC_EPOCH.ajd + @rc_ajd
-		self::from_ajd(@ajd)
-	end
 
 
 
