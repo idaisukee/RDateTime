e2a8242c089996de23305d3538889f6c8fb0ebdb
Daisuke Inoue <hibariyashiki@gmail.com>
2016-07-18 17:08:20 +0900

    10_date_table.rb

---
+++ 10_date_table.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../r_date_time.rb',  __FILE__)
+require File.expand_path('../float.rb',  __FILE__)
+require File.expand_path('../range.rb',  __FILE__)
+
+now = RDateTime.now
+yera = now.year
+min = RDateTime.new(now.year, 1, 1, 8, 0, 0, Rational(9, 24))
+max = RDateTime.new(now.year + 1, 1, 1, 8, 0, 0, Rational(9, 24))
+
+range = Range.new(min, max)
+
+range.each_n_day(10) do |d|
+
+	gc = sprintf('%02d-%02d', d.month, d.day)
+	prc = sprintf('%03d', d.prop_rc_day.floor)
+	puts "#{gc}\t#{prc}"
+	
+end
