299fde155b62d4b8cd3a03233884261526321a82
Daisuke Inoue <hibariyashiki@gmail.com>
2016-08-20 00:48:49 +0900

    calendar.rb

---
+++ calendar.rb
@@ -0,0 +1,28 @@
+require File.expand_path('../r_date_time.rb',  __FILE__)
+require File.expand_path('../float.rb',  __FILE__)
+require File.expand_path('../range.rb',  __FILE__)
+
+
+now = RDateTime.now
+
+year = now.year
+month = now.month
+day = now.day
+
+today = RDateTime.new(year, month, day, 10, 0)
+
+
+start = today - 15
+finish = today + 15
+
+range = Range.new(start, finish)
+
+range.each_n_day(1) do |day|
+	gc_array = [day.year, day.month, day.day]
+	prc_array = [day.prop_rc_year, day.prop_rc_day.floor]
+
+	gc_str = gc_array.join('/')
+	prc_str = prc_array.join(':')
+
+	puts gc_str + "\t" + prc_str
+end
