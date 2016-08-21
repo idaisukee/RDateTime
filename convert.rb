4012748415a210ccfcc6b29a5e5e756f3d73e2e8
Daisuke Inoue <hibariyashiki@gmail.com>
2016-08-21 16:23:38 +0900

    convert.rb

--- convert.rb
+++ convert.rb
@@ -7,14 +7,46 @@ array = str.split(' ')
 
 system = array[0]
 
+ret = RDateTime.r_parse(str)
+type = ret[0]
+obj = ret[1]
+out = Array.new
+
+
+
 case system
 when 'g'
+	if type.include? 'year' then
+		out << obj.prop_rc_year 
+	else
+		out << ''
+	end
+	day = obj.prop_rc_day
+	if type.include? 'time' then
+		out_day = day.to_f
+	else
+		out_day = day.floor
+	end
+	out << out_day
+	puts out.join(':')
+
 when 'pr'
+
+	date = Array.new
+	time = Array.new
+
+	date << obj.year if type.include? 'year'
+	date << obj.month if type.include? 'month'
+	date << obj.monthday if type.include? 'monthday'
+	date << obj.day if type.include? 'day'
+	if type.include? 'time' then
+		time << obj.hour 
+		time << obj.min
+	end
+	date_str = date.join('/')
+	time_str = time.join(':')
+	out = [date_str, time_str]
+	out_str = out.join('T')
+	puts out_str
+
 end
-
-
-out = RDateTime.r_parse(str)
-
-puts out
-
-
