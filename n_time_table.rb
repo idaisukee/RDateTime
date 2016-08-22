36ec906176e8e67282e72737cd6793392c4a8c95
Daisuke Inoue <hibariyashiki@gmail.com>
2016-08-22 11:11:47 +0900

    n_time_table.rb

---
+++ n_time_table.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../r_date_time.rb',  __FILE__)
+require File.expand_path('../float.rb',  __FILE__)
+require File.expand_path('../range.rb',  __FILE__)
+
+require 'optparse'
+
+available_opts = 'n:'
+params = ARGV.getopts(available_opts)
+n = params['n'].to_i
+
+min = RDateTime.new(2016, 1, 1, 8, 0, 0, Rational(9, 24))
+max = min + Rational(n, 24)
+
+range = Range.new(min, max)
+
+range.each_n_gc_min(15) do |t|
+
+	rc_date = t.prop_rc_day.floor
+	rc_time = t.prop_rc_day - rc_date
+	rc_time_i = (rc_time.floor(4) * 10 ** 4).to_i
+	rc = sprintf('.%04d', rc_time_i)
+
+
+	gc_hour = sprintf('%02d', t.hour)
+	gc_min = sprintf('%02d', t.min)
+	gc = "#{gc_hour}:#{gc_min}"
+	print "#{gc}\t#{rc}\n"
+
+end
