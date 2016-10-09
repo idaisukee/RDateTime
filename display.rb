36a83daf952f06f8eb84538222a9f5437285c5ad
Daisuke Inoue <hibariyashiki@gmail.com>
2016-10-09 15:12:02 +0900

    display.

--- display.rb
+++ display.rb
@@ -10,5 +10,5 @@ prop_rc_year = m.prop_rc_year
 season_ratio = sprintf('%2.2f', m.season_ratio.to_f.e_floor(2).to_f)
 prop_rc_day = sprintf('%3.3f', m.prop_rc_day.to_f.e_floor(3).to_f)
 
-str = "#{prop_rc_year}:#{season_ratio}|#{prop_rc_day}"
+str = "#{prop_rc_year}:#{prop_rc_day} ( #{season_ratio} )"
 print str
