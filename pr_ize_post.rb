d7a97821ac7d4bda9dc7b6f7d5b65a8574f561a9
Daisuke Inoue <hibariyashiki@gmail.com>
2016-11-08 10:19:05 +0900

    pr_ize_post

--- pr_ize_post.rb
+++ pr_ize_post.rb
@@ -22,10 +22,10 @@ File.open(file, 'r') do |file|
 			ry = r.prop_rc_year
 			rd = r.prop_rc_day.to_f.e_floor(3).to_f
 			str = ry.to_s + ':' + rd.to_s
-			out_str = '- time: ' + str
+			out_str = '- prd: ' + str
 			puts out_str
 			rc_ajd_str = r.rc_ajd.to_f.e_floor(3).to_f.to_s
-			out_rc_ajd_str = '- rc_ajd: ' + rc_ajd_str
+			out_rc_ajd_str = '  rc_ajd: ' + rc_ajd_str
 			puts out_rc_ajd_str
 		else
 			print line
