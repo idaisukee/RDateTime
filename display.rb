f13c9d50c37b7b101383e2a7e585fb162ead8f33
Daisuke Inoue <hibariyashiki@gmail.com>
2017-01-31 19:01:40 +0900

    use rc_ajd in display.rb .

--- display.rb
+++ display.rb
@@ -7,8 +7,9 @@ m = RDateTime.now
 str = "#{m.prop_rc_year}:#{m.season_ratio.to_f.e_floor(2).to_f}|#{m.prop_rc_day.to_f.e_floor(3).to_f}"
 
 prop_rc_year = m.prop_rc_year
+rc_ajd = m.rc_ajd.to_f
+rc_ajd_str = sprintf('%5.3f', rc_ajd)
 season_ratio = sprintf('%2.2f', m.season_ratio.to_f.e_floor(2).to_f)
-prop_rc_day = sprintf('%3.3f', m.prop_rc_day.to_f.e_floor(3).to_f)
 
-str = "#{prop_rc_year}:#{prop_rc_day} ( #{season_ratio} )"
+str = "#{rc_ajd_str} ( #{season_ratio} )"
 print str
