646215ec25c553da744907f302cf4749c5da191f
Daisuke Inoue <hibariyashiki@gmail.com>
2017-01-31 19:02:25 +0900

    delete unnec codes.

--- display.rb
+++ display.rb
@@ -4,7 +4,6 @@ require File.expand_path('../float.rb',  __FILE__)
 
 
 m = RDateTime.now
-str = "#{m.prop_rc_year}:#{m.season_ratio.to_f.e_floor(2).to_f}|#{m.prop_rc_day.to_f.e_floor(3).to_f}"
 
 prop_rc_year = m.prop_rc_year
 rc_ajd = m.rc_ajd.to_f
