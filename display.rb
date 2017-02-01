e51cbe329d16342ed1a7da6cd44da78287f8312b
Daisuke Inoue <hibariyashiki@gmail.com>
2017-02-01 16:54:39 +0900

    life_ratio etc.

--- display.rb
+++ display.rb
@@ -11,5 +11,15 @@ rc_ajd_str = sprintf('%5.3f', rc_ajd)
 season_ratio = m.season_ratio.to_f
 season_ratio_str = sprintf('%1.2f', season_ratio)
 
-str = "#{rc_ajd_str} ( #{season_ratio_str} )"
+AVR_LIFE = 80.79
+
+life_start = RDateTime.new(1983, 6, 9, 0, 0, 0, RDateTime::JST)
+life_end = life_start + 365.22 * AVR_LIFE
+life_length = life_end - life_start
+life_past = m - life_start
+life_ratio = life_past / life_length
+life_past_str = sprintf('%5.0f', life_past.floor)
+life_ratio_str = sprintf('%.3f', life_ratio)
+
+str = "#{rc_ajd_str} ( #{life_past_str} #{life_ratio_str} #{season_ratio_str} )"
 print str
